691f692fd166bff82f0bfc92f04f017d8cc3d21a
ShadyKahouech <shady.kahouech@gmail.com>
2024-10-27 12:48:05 +0100

    wishlist completed 90% still work on the delete function

--- client/src/component/wishList/WishList.jsx
+++ client/src/component/wishList/WishList.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { getWishlist } from "../service/serviceWishlist";
+import MainNavbar from "../navbar/MainNavbar";
+import { FaTrashAlt } from "react-icons/fa";
+import "./WishList.css";
 
 function WishList() {
   const [userWishlist, setUserWishlist] = useState(null);
   const [wishlistProduct, setWishlistProduct] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -32,26 +37,40 @@ function WishList() {
 
   return (
     <div>
-      <h2>Wishlist</h2>
-      {wishlistProduct.length > 0 ? (
-        wishlistProduct.map((item, index) => (
-          <div key={index}>
-            <h3>Product: {item.Product?.name || "Unnamed Product"}</h3>
-            <p>Price: {item.Product?.price}</p>
-            <p>Description: {item.Product?.description}</p>
-            <p>Category: {item.Product?.category}</p>
-            <p>Average Rating: {item.Product?.averageRating}</p>
-            <p>Ratings Count: {item.Product?.ratingsCount}</p>
-            <img
-              src={item.Product?.images}
-              alt={item.Product?.name}
-              style={{ width: "100px", height: "100px" }}
-            />
-          </div>
-        ))
-      ) : (
-        <div>No products in wishlist</div>
-      )}
+      <MainNavbar />
+
+      <div className="wishlist__grid">
+        {wishlistProduct.length > 0 ? (
+          wishlistProduct.map((item, index) => (
+            <div key={index} className="wishlist__container">
+              <div className="delete__image__wishlist">
+                <img
+                  className="img__product__wishlist"
+                  src={item.Product?.images[0]}
+                  alt={item.Product?.name}
+                  style={{ width: "150px", height: "150px" }}
+                />
+                <FaTrashAlt
+                  className="delete-icon"
+                  // onClick={() => handleDelete(item.Product?.productId)}
+                  title="Remove from wishlist"
+                />
+              </div>
+              <div className="product__details__wishlist">
+                <h3>Product: {item.Product?.name}</h3>
+                <p>Price: {item.Product?.price}</p>
+                <p>Description: {item.Product?.description}</p>
+                <p>Category: {item.Product?.category}</p>
+              </div>
+              <button className="pannier__button__wishlist">
+                Ajouter au pannier
+              </button>
+            </div>
+          ))
+        ) : (
+          <div>No products in wishlist</div>
+        )}
+      </div>
     </div>
   );
 }
